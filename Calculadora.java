a4ff006ecad612305e3789f4f1a0bc97ecc2c259
emar91 <142558011+emar91@users.noreply.github.com>
2023-08-17 23:10:24 -0600

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package calculadora.main;
+
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        graficCALC frm = new graficCALC ();
+        frm.show();
+        
+    }
+}
